b08f4e15c392189dbf756804f611bf0451aab4eb
Amir <mirfallahi2009@gmail.com>
2023-01-14 20:43:23 +0330

    first commit

--- messanger/chats/views.py
+++ messanger/chats/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-import time
 from .models import (
     Messages
 )
